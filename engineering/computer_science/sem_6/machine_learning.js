e102812926288d9a320d090e634570faf90db1f6
ar739900 <71808370+ar739900@users.noreply.github.com>
2022-04-28 17:37:08 +0530

    machine learning done

---
+++ engineering/computer_science/sem_6/machine_learning.js
@@ -0,0 +1,42 @@
+module.exports = course = {
+    outline:
+        'No lab work in this module',
+    objecttives: [
+        '• To learn the concept of how to learn patterns and concept from data. \n• Design and analyze various machine learning algorithms and their applications in recent trends. \n• Evaluate the various factors of machine learning to measure the performance. \n• Understand basic of machine learning’s application in recent trend of technology.',
+    ],
+    name: 'Machine Learning',
+    code: 'PCC CS 603',
+    credits: 4,
+    content: [
+        {
+            module: '1',
+            topics: 'Introduction: Basic definitions, Linear Algebra, Statistical learning theory, types of learning, hypothesis space and Inductive bias, evaluation and cross validation, Optimization. ',
+        },
+        {
+            module: '2',
+            topics: 'Statistical Decision Theory, Bayesian Learning (ML, MAP, Bayes estimates, Conjugate priors), Linear Regression, Ridge Regression, Lasso, Principal Component Analysis, Partial Least Squares',
+        },
+        {
+            module: '3',
+            topics: 'Linear Classification, Logistic Regression, Linear Discriminant Analysis, Quadratic Discriminant \nAnalysis, Perceptron, Support Vector Machines + Kernels, Artificial Neural Networks + Back \nPropagation, Decision Trees, Bayes Optimal Classifier, Naive Bayes.',
+        },
+        {
+            module: '4',
+            topics: 'Hypothesis testing, Ensemble Methods, Bagging Adaboost Gradient Boosting, Clustering, K-means, K-medoids, Density-based Hierarchical, Spectral. ',
+        },
+        {
+            module: '5',
+            topics: 'Expectation Maximization, GMMs, Learning theory Intro to Reinforcement Learning, Bayesian Networks.',
+        },
+    ],
+    books: [
+        { name: ' Machine Learning. First Edition', author: 'Tom Mitchell.' },
+        { name: ' Introduction to Machine Learning Edition 2', author: ' Ethem Alpaydin ' },
+        { name: 'Readings in Machine Learning', author: ' J. Shavlik and T. Dietterich (Ed)' },
+        { name: 'Elements of Machine Learning', author: ' P. Langley,Morgan Kaufmann' },
+    ],
+
+    outcomes: [
+        'Not available',
+    ],
+};
