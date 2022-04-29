71bb563cd3b58482681ba2b583565141f4fdd3d9
ar739900 <71808370+ar739900@users.noreply.github.com>
2022-04-29 17:09:39 +0530

    software engineering done

---
+++ engineering/computer_science/sem_5/software_engineering.js
@@ -0,0 +1,52 @@
+module.exports = course = {
+    outline:
+        '',
+    objecttives: [
+        '',
+    ],
+    name: 'Software Engineering ',
+    code: 'PCC CS 504',
+    credits: 3,
+    content: [
+        {
+            module: '1',
+            topics: 'Introduction: What is Software Engineering and its history, software crisis, Evolution of a Programming System Product, Characteristics of Software, Brooks’ No Silver Bullet, and Software Myths, Software Development Life Cycles: Software Development Process, The Code-and-Fix model, The Waterfall model, The Evolutionary Model, The Incremental Implementation, Prototyping, The Spiral Model, Software Reuse, Critical Comparisons of SDLC models, An Introduction to Non-Traditional Software Development Process: Rational Unified Process, Rapid Application Development, Agile Development Process.',
+        },
+        {
+            module: '2',
+            topics: 'Requirements: Importance of Requirement Analysis, User Needs, Software Features and Software Requirements, Classes of User Requirements: Enduring and Volatile, Sub phases of Requirement Analysis, Functional and Nonfunctional requirements, Barriers to Eliciting User requirements, The software requirements document and SRS standards, Requirements Engineering, Case Study of SRS for a Real Time System. Tools for Requirements Gathering: Document Flow Chart, Decision Table, Decision Tree, Introduction to nontraditional Requirements',
+        },
+        {
+            module: '3',
+            topics: 'Software Design: Goals of good software design, Design strategies and methodologies, Data oriented software design, Coupling, Cohesion, Modular structure, Packaging, Structured Analysis: DFD, Data Dictionary, Structured Design: Structure chart, Object oriented design, Top-down and bottom-up approach, UML, UML Diagrams, Design patterns,.',
+        },
+        {
+            module: '4',
+            topics: 'Software Project Management: Overview of Project Manager Responsibilities & project planning, Software Measurement and Metrics: Line of Code (LOC), Function Point (FP) based measures, Various Size Oriented Measures: Halstead s software science, Project Size estimation Metrics Project Estimation, Techniques, COCOMO, Staffing Level Estimation, Scheduling, Organization & Team Structures Staffing, Risk Management.',
+        },
+        {
+            module: '5',
+            topics: 'Software Coding & Testing: Development: Selecting a language, Coding guidelines, Writing code, Code documentation. Testing process, Design of test cases, Functional Testing: Boundary value analysis, Equivalence class testing, Decision table testing, Cause effect graphing, Structural testing, Cyclomatic Complexity Measures: Control flow graphs, Path testing, Data flow and mutation testing, Unit testing, Integration and system testing, Debugging, Alpha & beta testing, testing tools & standards.',
+        },
+        {
+            module: '6',
+            topics: 'Software Maintenance: Management of maintenance, Maintenance process, Maintenance models, Regression testing, Reverse engineering, Software reengineering, Configuration management, documentation.',
+        },
+        {
+            module: '7',
+            topics: 'Software Reliability & Quality Management: Introduction to reliability and metrics to reliability measure, Overview of S/W Quality management System ISO 9000, SEI CMM.',
+        }
+    ],
+    books: [
+        { name: "Software Engineering: A Practitioners Approach", author: 'R. S. Pressman' },
+        { name: ' Fundamental of Software Engg.', author: 'By Rajib Mall' },
+        { name: 'Zero Defect Software', author: 'G. G. Schulmeyer' },
+        { name: 'Object Oriented Modeling and Design', author: ', J. Rumbaugh' },
+    ],
+
+    outcomes: [
+        '',
+        '',
+        '',
+    ],
+};
