875fc74bac73c6ae92d2e336fdf6446d1afccf82
ar739900 <71808370+ar739900@users.noreply.github.com>
2022-04-29 17:31:38 +0530

    coi done

---
+++ engineering/computer_science/sem_5/constitution_of_india.js
@@ -0,0 +1,84 @@
+module.exports = course = {
+    outline:
+        '',
+    objecttives: [
+        'The Constitution of India is the supreme law of India. Parliament of India cannot make any law which violates the Fundamental Rights enumerated under the Part III of the Constitution. The Parliament of India has been empowered to amend the Constitution under Article 368, however, it cannot use this power to change the “basic structure” of the constitution, which has been ruled and explained by the Supreme Court of India in its historical judgments. The Constitution of India reflects the idea of “Constitutionalism” – a modern and progressive concept historically developed by the thinkers of “liberalism” – an ideology which has been recognized as one of the most popular political ideology and result of historical struggles against arbitrary use of sovereign power by state. The historic revolutions in France, England, America and particularly European Renaissance and Reformation movement have resulted into progressive legal reforms in the form of “constitutionalism” in many countries. The Constitution of India was made by borrowing models and principles from many countries including United Kingdom and America.\n     The Constitution of India is not only a legal document but it also reflects social, political and economic perspectives of the Indian Society. It reflects India’s legacy of “diversity”. It has been said that Indian constitution reflects ideals of its freedom movement, however, few critics have argued that it does not truly incorporate our own ancient legal heritage and cultural values. No law can be “static” and therefore the Constitution of India has also been amended more than one hundred times. These amendments reflect political, social and economic developments since the year 1950. The Indian judiciary and particularly the Supreme Court of India has played an historic role as the guardian of people. It has been protecting not only basic ideals of the Constitution but also strengthened the same through progressive interpretations of the text of the Constitution. The judicial activism of the Supreme Court of India and its historic contributions has been recognized throughout the world and it gradually made it “as one of the strongest court in the world”.',
+    ],
+    name: 'Constitution of India – Basic features and fundamental principles',
+    code: 'MC 501',
+    credits: 0,
+    content: [
+        {
+            module: '1',
+            topics: ' Meaning of the constitution law and constitutionalism',
+        },
+        {
+            module: '2',
+            topics: 'istorical perspective of the Constitution of India',
+        },
+        {
+            module: '3',
+            topics: ' Salient features and characteristics of the Constitution of India            ',
+        },
+        {
+            module: '4',
+            topics: 'Scheme of the fundamental rights',
+        },
+        {
+            module: '5',
+            topics: 'The scheme of the Fundamental Duties and its legal status            ',
+        },
+        {
+            module: '6',
+            topics: 'The Directive Principles of State Policy – Its importance and implementation',
+        },
+        {
+            module: '7',
+            topics: 'Federal structure and distribution of legislative and financial powers between the Union and the States',
+        },
+        {
+            module: '8',
+            topics: 'Parliamentary Form of Government in India – The constitution powers and status of the President of India',
+        },
+        {
+            module: '9',
+            topics: 'Amendment of the Constitutional Powers and Procedure            ',
+        },
+        {
+            module: '10',
+            topics: 'The historical perspectives of the constitutional amendments in India            ',
+        },
+        {
+            module: '11',
+            topics: 'Emergency Provisions: National Emergency, President Rule, Financial Emergency',
+        },
+        {
+            module: '12',
+            topics: 'Local Self Government – Constitutional Scheme in India ',
+        },
+        {
+            module: '13',
+            topics: 'Scheme of the Fundamental Right to Equality            ',
+        },
+        {
+            module: '14',
+            topics: 'Scheme of the Fundamental Right to certain Freedom under Article 19',
+        },
+        {
+            module: '15',
+            topics: '. Scope of the Right to Life and Personal Liberty under Article 21.',
+        },
+    ],
+    books: [
+        { name: '', author: '' },
+        { name: '', author: '' },
+        { name: '', author: '' },
+        { name: '', author: '' },
+    ],
+
+    outcomes: [
+        '',
+        '',
+        '',
+    ],
+};
