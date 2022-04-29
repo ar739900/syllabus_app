06043c3b9c36912ef5e86aca888d49af69165f6c
ar739900 <71808370+ar739900@users.noreply.github.com>
2022-04-29 17:18:30 +0530

    professional skill development done

--- engineering/computer_science/sem_5/professional_skill_development.js
+++ engineering/computer_science/sem_5/professional_skill_development.js
@@ -2,31 +2,40 @@ module.exports = course = {
     outline:
         '',
     objecttives: [
-        '',
+        '1. To learn various interpersonal skills \n2. To help in developing various professionals skills.\n3. To cover the facets of verbal and non-verbal languages, public speech, reading gestures and body languages, preparing for group discussion and enhancing presentations skills.\n4. To enable learners to speak fluently and flawlessly in all kinds of communicative Contexts with speakers of all nationalities.',
     ],
-    name: '',
-    code: '',
-    credits: 5,
+    name: 'Professional Skill Development',
+    code: 'HSMC 501',
+    credits: 3,
     content: [
         {
-            module: '',
-            topics: '',
+            module: '1',
+            topics: 'Communication skills: Public speaking, Group discussion, Gestures and body language & professional presentation skills',
         },
         {
-            module: '',
-            topics: '',
+            module: '2',
+            topics: 'Interpersonal skills: Group dynamics, Negotiation skills, Leadership, Emotional intelligence',
+        },
+        {
+            module: '3',
+            topics: 'Employability and Corporate Skills: Time management and effective planning, Stress management, People skills, Team work, development of leadership qualities, Decision making and Negotiation skills, Positive attitude, Self-motivation, Professional ethics, Business etiquettes, balancing board room.',
+        },
+        {
+            module: '4',
+            topics: 'Business writing skills, Resume Writing. Interview Skills, Technical Presentation, Guest Lecture, Professional Ethics, Project Management, Entrepreneurship.',
         },
     ],
     books: [
-        { name: '', author: '' },
-        { name: '', author: '' },
-        { name: '', author: '' },
-        { name: '', author: '' },
+        { name: '“Personality Development and Soft Skills”', author: 'Barun Mitra' },
+        { name: '“Managing Soft Skills for Personality Development”', author: ' B.N. Ghosh' },
+        { name: ' “Communication Skills and Soft Skills: An Integrated Approach”', author: 'E. Suresh Kumar' },
+        { name: '“Communication to Win”', author: ' Richard Denny' },
     ],
 
     outcomes: [
-        '',
-        '',
-        '',
+        '1. Student can able to write their resume and can prepare for presentation, group discussion and interview.',
+        '2. Student can develop interpersonal skills like negotiation and leadership skills.',
+        '3. Students can develop Employability and Corporate Skills with proper time management and stress management.',
+        '4. Students learn to practice the professional ethics, project management and Entrepreneurship.',
     ],
 };
