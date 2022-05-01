cd08ff834d03e1bca18a280af609edb62058f30a
ar739900 <71808370+ar739900@users.noreply.github.com>
2022-05-01 18:24:47 +0530

    technical writing done

---
+++ engineering/computer_science/sem_3/Technical_Writing.js
@@ -0,0 +1,40 @@
+module.exports = course = {
+    outline:
+        '*******',
+    objecttives: [
+        '1. To understand the variety of structure of technical documents.\n2. To convey clearly, cogently and correctly, through written media, the technical aspects of a practice to audiences.\n3. To recognize and use of the verbal and technical elements necessary for the successful practice of scientific and technical communication.\n4. To work collaboratively and individually to research, to analyze, and to write about, public debates regarding the conduct of science and technology',
+    ],
+    name: 'Technical Writing',
+    code: 'HSMC 301',
+    credits: 3,
+    content: [
+        {
+            module: '1',
+            topics: 'Introduction: Fundamentals of Technical Writing: Need for Clear and Concise Technical Writing, Attributes of Technical Writing, Types of Technical Writing, Benefits of Technical Writing, Technical, Managerial and General Readers, Expressing versus Impressing, Correct use of Noun, Pronoun, Verb, Adjective, Adverbs, Tense and Punctuation.',
+        },
+        {
+            module: '2',
+            topics: 'Performing Technical Studies: Types of Technical Studies, General Methodology- Proposing a Project,Gathering Background Information, Designing Test Plans, Performing Experiments, Reporting Results. Writing Strategy: Analysis of Readers, Scope of Writing, Purpose and Objective. Document Options: Document Hierarchy, Report Types and Selection. Criteria for Good Technical Writing: Technical Content, Presentation, Language Skills. Writing Style: Elements of Style, Examples of Writing Styles, Recommended Style, Learn to Prepare Effective Illustrations.',
+        },
+        {
+            module: '3',
+            topics: 'Formal Reports: The Outline and Introduction (Outline, Title, Front Matter, Writing the Introduction), Writing the Body (Writing a Procedure, Describing Machines/Processes, Writing Test Results, Writing the Discussion Section), Closure (Conclusions, Recommendations, References, Abstract, Back Matter, Report Distribution, Saving Reports). Informal Reports: Elements of an Informal Report, Investigation Reports, Service Work, Action Letters and Proposals. Typical Memo Reports. ',
+        },
+        {
+            module: '4',
+            topics: 'Review and Editing: Types of Review and Edit, Review and Editing Methodology, Examples of Reviews. Oral Presentations: Types of Oral Presentations, Preparation, Visual Aids, Impediments to Technical Writing, Maintaining Writing Skills, Measuring Report Results. ',
+        },
+    ],
+    books: [
+        { name: 'Engineers’ Guide to Technical Writing', author: 'Kenneth G. Budinski' },
+        { name: 'Handbook for Technical Writing', author: 'James H. Shelton' },
+        { name: "The Technical Writer's Handbook: Writing With Style and Clarity", author: 'Matt Young' },
+    ],
+
+    outcomes: [
+        '1. Student should able to demonstrate improved competence in Standard Written English, including grammar, sentence and paragraph structure, coherence, and document design (including the use of the visual), and use this knowledge to revise texts.',
+        '2. Student should identify and practice the stages required to produce competent, professional writing through planning, drafting, revising, and editing.',
+        '3. It determine and implement the appropriate methods for each technical writing task.',
+        '4. Students learn to practice the ethical use of sources and the conventions of citation appropriate to each genre.',
+    ],
+};
