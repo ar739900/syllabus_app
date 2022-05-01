3f0aa0fd70b7b66d7622b8965f5a6f69f629ba6e
ar739900 <71808370+ar739900@users.noreply.github.com>
2022-05-01 18:15:52 +0530

    math 3 done

---
+++ engineering/computer_science/sem_3/mathematics3.js
@@ -0,0 +1,43 @@
+module.exports = course = {
+    outline:
+        '*********',
+    objecttives: [
+        '********',
+    ],
+    name: 'Mathematics-III (Differential Calculus) ',
+    code: 'BSC 301',
+    credits: 2,
+    content: [
+        {
+            module: '1',
+            topics: 'Successive Differentiation, Leibnitz’s Theorem. Limit, Continuity and Differentiability of function for one variable.',
+        },
+        {
+            module: '2',
+            topics: 'Limit, Continuity and Differentiability of function for several variables. Partial Derivatives, Euler’s Theorem for Homogeneous functions, Total derivatives, Change of Variables. Maxima and Minima of Several Variables. Methods of Lagrange Multipliers. Taylor’s and Maclaurin’s Theorem with remainders of several variables.',
+        },
+        {
+            module: '3',
+            topics: 'Vector Calculus: Gradient, Divergence and Curl of a Vector and their Physical Interpretations, Vector Identities. Directional Derivatives. Line, Surface and Volume integrals, Application of Green’s, Stokes and Gauss Divergence Theorem (Without Proof).',
+        },
+        {
+            module: '4',
+            topics: 'First Order Ordinary Differential Equations: Exact, Linear and Bernoulli’s Equations, Euler’s Equations, Equations not of First Degree: Equations Solvable for P, Equations Solvable for Y, Equations Solvable for X and Clairaut’s Type.',
+        },
+        {
+            module: '5',
+            topics: 'Ordinary Differential Equations of Higher Orders: Second Order Linear Differential Equations with Variable Coefficients, Method of Variation of Parameters, Cauchy-Euler Equation; Power Series Solutions; Legendre Polynomials, Bessel Functions of the First Kind and their properties.',
+        },
+        {
+            module: '6',
+            topics: 'Partial Differential Equations – First Order: First Order Partial Differential Equations, Solutions of First Order Linear and Non-Linear PDEs.',
+        },
+    ],
+    books: [
+        { name: 'Higher Engineering Mathematics', author: 'B S Grewal' },
+    ],
+
+    outcomes: [
+        '********',
+    ],
+};
