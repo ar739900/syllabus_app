41c0c2852a6b97246d27f8fcc25f0ea481cb727a
ar739900 <71808370+ar739900@users.noreply.github.com>
2022-05-01 18:01:48 +0530

    oops done

---
+++ engineering/computer_science/sem_3/oops_using_cpp.js
@@ -0,0 +1,47 @@
+module.exports = course = {
+    outline:
+        'Hands-on experiments related to the course contents of PCC CS 302.',
+    objecttives: [
+        '1. To impart the basic concepts of Object Oriented Programming.\n2. To understand concepts about Classes and Data Abstraction. \n3. To understand basic concepts about Inheritance.\n4. To enable them to write algorithms for solving problems using object oriented approach.',
+    ],
+    name: 'Object Oriented Programming using C++ and Object Oriented Programming using C++ Lab',
+    code: 'PCC CS 302 & PCC CS 302P',
+    credits: 5,
+    content: [
+        {
+            module: '1',
+            topics: 'Introduction to C++ : Object Oriented Technology, Advantages of OOP, Input- output in C++, Tokens, Keywords, Identifiers, Data Types C++, Derives data types. The void data type, Type Modifiers, Typecasting, Constant, Operator, Precedence of Operators, Strings.',
+        },
+        {
+            module: '2',
+            topics: 'Control Structures and Functions: Decision making statements like if-else, Nested if-else, goto, break, continue, switch case, Loop statement like for loop, nested for loop, while loop, do-while loop. Parts of Function, User- defined Functions, Value- Returning Functions, void Functions, Value Parameters, Function overloading, Virtual Functions.',
+        },
+        {
+            module: '3',
+            topics: 'Classes and Data Abstraction : Structure in C++, Class, Build- in Operations on Classes, Assignment Operator and Classes, Class Scope, Reference parameters and Class Objects (Variables), Member functions, Accessor and Mutator Functions, Constructors, default Constructor, Destructors.',
+        },
+        {
+            module: '4',
+            topics: 'Overloading, Templates and Inheritance: Operator Overloading, Function Overloading, Function Templates, Class Templates. Single and Multiple Inheritance, virtual Base class, Abstract Class, Pointer and Inheritance, Overloading Member Function.',
+        },
+        {
+            module: '5',
+            topics: 'Pointers, Arrays and Exception Handling: Void Pointers, Pointer to Class, Pointer to Object, Void Pointer, Arrays. The keywords try, throw and catch. Creating own Exception Classes, Exception Handling Techniques (Terminate the Program, Fix the Error and Continue, Log the Error and Continue), Stack Unwinding',
+        },
+    ],
+    books: [
+        { name: 'Thinking in C++, Volume 1 & 2', author: 'Bruce Eckel, Chuck Allison' },
+        { name: 'Mastering C++, 1/e', author: 'Venugopal' },
+        { name: 'Object Oriented Programming with C++, 3/e', author: 'E. Balaguruswamy' },
+        { name: 'Starting Out with Object Oriented Programming in C++', author: 'Tony Gaddis' },
+    ],
+
+    outcomes: [
+        '1. Understand the concepts of Class, Object, Inheritance and Polymorphism.',
+        '2. Apply overload operators in C++',
+        '3. Understand the difference between function overloading and function overriding ',
+        '4. Incorporate exception handling in object-oriented programs',
+        '5. Able to use template classes.',
+        '6. Able to write object-oriented programs of moderate complexity in C++',
+    ],
+};
